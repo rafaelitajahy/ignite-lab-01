d001c3b1eccd1182cbfdd487d0a9a3d524136735
Rafael Itajahy <rafaelitajahy@gmai.com>
2022-05-04 23:57:11 -0300

    updated classroom

--- classroom/src/http/auth/authorization.guard.ts
+++ classroom/src/http/auth/authorization.guard.ts
@@ -1,26 +1,26 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import jwt from 'express-jwt';
-import {expressJwtSecret} from 'jwks-rsa';
+import { expressJwtSecret } from 'jwks-rsa';
 import { promisify } from 'node:util';
-
 
 @Injectable()
 export class AuthorizationGuard implements CanActivate {
   private AUTH0_AUDIENCE: string;
   private AUTH0_DOMAIN: string;
 
-  constructor(private configService: ConfigService){
+  constructor(private configService: ConfigService) {
     this.AUTH0_AUDIENCE = this.configService.get('AUTH0_AUDIENCE') ?? '';
     this.AUTH0_DOMAIN = this.configService.get('AUTH0_DOMAIN') ?? '';
   }
 
-  async canActivate(context: ExecutionContext):  Promise<boolean>  {
-    // const httpContext = context.switchToHttp();
-    // const req = httpContext.getRequest();
-    // const res = httpContext.getResponse();
-
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const { req, res } = GqlExecutionContext.create(context).getContext();
 
     const checkJWT = promisify(
@@ -29,11 +29,11 @@ export class AuthorizationGuard implements CanActivate {
           cache: true,
           rateLimit: true,
           jwksRequestsPerMinute: 5,
-          jwksUri: `${this.AUTH0_DOMAIN}.well-known/jwks.json`
-    }),
+          jwksUri: `${this.AUTH0_DOMAIN}.well-known/jwks.json`,
+        }),
         audience: this.AUTH0_AUDIENCE,
         issuer: this.AUTH0_DOMAIN,
-        algorithms: ['RS256']
+        algorithms: ['RS256'],
       }),
     );
 
@@ -42,7 +42,7 @@ export class AuthorizationGuard implements CanActivate {
 
       return true;
     } catch (err) {
-      throw new UnauthorizedException
-    }   
+      throw new UnauthorizedException(err);
+    }
   }
 }
--- classroom/src/http/http.module.ts
+++ classroom/src/http/http.module.ts
@@ -1,21 +1,40 @@
-import { ApolloDriver } from '@nestjs/apollo';
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
-import path from 'path';
-
-import { DatabaseModule } from '../database/database.module';
-import { TestResolver } from './test.resolver';
-
-@Module({
+import {
+    ApolloDriver,
+    ApolloFederationDriver,
+    ApolloFederationDriverConfig,
+  } from '@nestjs/apollo';
+  import { Module } from '@nestjs/common';
+  import { ConfigModule } from '@nestjs/config';
+  import { GraphQLModule } from '@nestjs/graphql';
+  import path from 'node:path';
+  
+  import { DatabaseModule } from '../database/database.module';
+  import { CoursesService } from '../services/courses.service';
+  import { EnrollmentsService } from '../services/enrollments.service';
+  import { StudentsService } from '../services/students.service';
+  import { CoursesResolver } from './graphql/resolvers/courses.resolver';
+  import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
+  import { StudentsResolver } from './graphql/resolvers/students.resolver';
+  
+  @Module({
     imports: [
-        ConfigModule.forRoot(),
-        DatabaseModule,
-        GraphQLModule.forRoot({
-            driver: ApolloDriver,
-            autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql')
-        })
+      ConfigModule.forRoot(),
+      DatabaseModule,
+      GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+        driver: ApolloFederationDriver,
+        autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+      }),
     ],
-    providers: [TestResolver]
-})
-export class HttpModule {}
+    providers: [
+      // Resolvers
+      CoursesResolver,
+      EnrollmentsResolver,
+      StudentsResolver,
+  
+      // Services
+      CoursesService,
+      EnrollmentsService,
+      StudentsService,
+    ],
+  })
+  export class HttpModule {}
